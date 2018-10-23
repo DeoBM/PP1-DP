ad8b87ed9442d3007cfe4c2a6e1263e7efe301be
DeoBM <21700398@handong.edu>
2018-10-24 01:30:08 +0900

    Task3 Add the test case for the 'Java-Algorithms' arff file:)

--- src/test/java/edu/handong/csee/pp1/dp/CLAMITest.java
+++ src/test/java/edu/handong/csee/pp1/dp/CLAMITest.java
@@ -66,6 +66,17 @@ public class CLAMITest {
 	}
 	
 	@Test
+	public void testCLAMIWithJavaAlgorithmsProject() {
+		
+		// TASK3: Conduct defect prediction by using CLAMI models
+		// complete this test case for the generated arff files from Task 2
+		
+		String[] args = {"-f","data/Java-Algorithms.arff","-l","label","-p", "buggy", "-m"};
+		
+		CLAMI.main(args);
+	}
+	
+	@Test
 	public void testCLAMIWithApacheProjectToFindBestCutoffForPrecision() {
 		
 		// TASK5 TODO Use this method to find the best cutoff
